806d0a09cf31bfdceb202c7b413e3fd9f0ac9e6d
Agnish Ghosh <iamagnix@gmail.com>
2024-11-13 16:25:10 +0700

    disable abandoned block verifer conditions

--- beacon_chain/sync/request_manager.nim
+++ beacon_chain/sync/request_manager.nim
@@ -541,7 +541,7 @@ proc getMissingDataColumns(rman: RequestManager): seq[DataColumnIdentifier] =
           debug "Not handling missing data columns early in slot"
           continue
 
-        if rman.dataColumnQuarantine[].hasMissingDataColumns(forkyBlck):
+        if not rman.dataColumnQuarantine[].hasMissingDataColumns(forkyBlck):
           let missing = rman.dataColumnQuarantine[].dataColumnFetchRecord(forkyBlck)
           if len(missing.indices) == 0:
             warn "quarantine is missing data columns, but missing indices are empty",
@@ -560,17 +560,17 @@ proc getMissingDataColumns(rman: RequestManager): seq[DataColumnIdentifier] =
             if id.index in local_custody and id notin fetches and 
                 len(forkyBlck.message.body.blob_kzg_commitments) != 0:
               fetches.add(id)
-        else:
-          # this is a programming error and it not should occur
-          warn "missing data column handler found columnless block with all data columns",
-             blk = columnless.root,
-             commitments=len(forkyBlck.message.body.blob_kzg_commitments)
-          ready.add(columnless.root)
+  #       else:
+  #         # this is a programming error and it not should occur
+  #         warn "missing data column handler found columnless block with all data columns",
+  #            blk = columnless.root,
+  #            commitments=len(forkyBlck.message.body.blob_kzg_commitments)
+  #         ready.add(columnless.root)
   
-  for root in ready:
-    let columnless = rman.quarantine[].popColumnless(root).valueOr:
-      continue
-    discard rman.blockVerifier(columnless, false)
+  # for root in ready:
+  #   let columnless = rman.quarantine[].popColumnless(root).valueOr:
+  #     continue
+  #   discard rman.blockVerifier(columnless, false)
   fetches
 
 proc requestManagerDataColumnLoop(
